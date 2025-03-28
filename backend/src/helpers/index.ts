a0c4f8ac1b66cb5d73e0aad19f8fb74376e95b07
juancarmore <juancar_more2@hotmail.com>
2025-03-28 16:04:45 +0100

    backend: Update helper exports to include password and Redis helpers

--- backend/src/helpers/index.ts
+++ backend/src/helpers/index.ts
@@ -1,3 +1,5 @@
 export * from './recording.helper.js';
 export * from './ov-components-adapter.helper.js';
 export * from './room.helper.js';
+export * from './password.helper.js';
+export * from './redis.helper.js';
