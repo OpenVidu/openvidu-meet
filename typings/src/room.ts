81db83129b51047a3b12fe482d1094399868761e
juancarmore <juancar_more2@hotmail.com>
2025-04-12 13:42:24 +0200

    typings: Add MeetRoomRoleAndPermissions interface

--- typings/src/room.ts
+++ typings/src/room.ts
@@ -1,11 +1,13 @@
+import { ParticipantPermissions, ParticipantRole } from './participant.js';
 import { MeetRoomPreferences } from './room-preferences.js';
 
 interface BaseRoomOptions {
-	autoDeletionDate?: number;
-	roomIdPrefix?: string;
-	preferences?: MeetRoomPreferences;
-	// maxParticipants?: number | null;
+    autoDeletionDate?: number;
+    roomIdPrefix?: string;
+    preferences?: MeetRoomPreferences;
+    // maxParticipants?: number | null;
 }
+
 /**
  * Options for creating or configuring a room.
  */
@@ -15,15 +17,20 @@ export type MeetRoomOptions = BaseRoomOptions;
  * Interface representing the response received when a room is created.
  */
 export interface MeetRoom extends BaseRoomOptions {
-	roomId: string;
-	creationDate: number;
-	moderatorRoomUrl: string;
-	publisherRoomUrl: string;
-	markedForDeletion?: boolean;
+    roomId: string;
+    creationDate: number;
+    moderatorRoomUrl: string;
+    publisherRoomUrl: string;
+    markedForDeletion?: boolean;
+}
+
+export interface MeetRoomRoleAndPermissions {
+    role: ParticipantRole;
+    permissions: ParticipantPermissions;
 }
 
 export type MeetRoomFilters = {
-	maxItems?: number;
-	nextPageToken?: string;
-	fields?: string;
+    maxItems?: number;
+    nextPageToken?: string;
+    fields?: string;
 };
