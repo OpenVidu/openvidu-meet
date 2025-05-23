de83220e094463fc76a6636554968ed81868f2d1
juancarmore <juancar_more2@hotmail.com>
2025-05-23 12:59:38 +0200

    frontend: enhance RoomRecordingsComponent with improved UI and functionality for managing recordings

--- frontend/projects/shared-meet-components/src/lib/pages/room-recordings/room-recordings.component.ts
+++ frontend/projects/shared-meet-components/src/lib/pages/room-recordings/room-recordings.component.ts
@@ -1,35 +1,54 @@
-import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule, DatePipe, DecimalPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '@lib/services';
-import {
-	OpenViduComponentsModule,
-	ApiDirectiveModule,
-	RecordingDeleteRequestedEvent
-} from 'openvidu-components-angular';
-import { MeetRecordingInfo } from 'shared-meet-components';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute } from '@angular/router';
+import { ContextService, HttpService } from '@lib/services';
+import { ActionService, MeetRecordingInfo, OpenViduComponentsUiModule } from 'shared-meet-components';
 
 @Component({
 	selector: 'app-room-recordings',
 	templateUrl: './room-recordings.component.html',
 	styleUrls: ['./room-recordings.component.scss'],
 	standalone: true,
-	imports: [OpenViduComponentsModule, ApiDirectiveModule]
+	imports: [
+		CommonModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatButtonModule,
+		MatIconModule,
+		DatePipe,
+		DecimalPipe,
+		OpenViduComponentsUiModule
+	]
 })
 export class RoomRecordingsComponent implements OnInit {
+	roomId = '';
+	canDeleteRecordings = false;
 	recordings: MeetRecordingInfo[] = [];
-	loading = true;
-	error = '';
-	private continuationToken: string | undefined;
+	moreRecordings = false;
+	private nextPageToken: string | undefined;
 
-	constructor(private httpService: HttpService) {}
+	constructor(
+		protected contextService: ContextService,
+		protected httpService: HttpService,
+		protected actionService: ActionService,
+		protected route: ActivatedRoute
+	) {}
 
 	async ngOnInit() {
+		this.route.params.subscribe((params) => {
+			this.roomId = params['room-id'];
+		});
+		this.canDeleteRecordings = this.contextService.canDeleteRecordings();
+
 		await this.loadRecordings();
-		console.log('Recordings loaded:', this.recordings);
 	}
 
-	async onLoadMoreRecordingsRequested() {
-		if (!this.continuationToken) {
+	async loadMoreRecordings() {
+		if (!this.moreRecordings || !this.nextPageToken) {
 			console.warn('No more recordings to load');
 			return;
 		}
@@ -37,27 +56,70 @@ export class RoomRecordingsComponent implements OnInit {
 		await this.loadRecordings();
 	}
 
-	async onRefreshRecordingsClicked() {
+	async refreshRecordings() {
+		this.resetRecordings();
 		await this.loadRecordings();
 	}
 
-	async onDeleteRecordingClicked(deleteRequest: RecordingDeleteRequestedEvent) {
+	playRecording(recording: MeetRecordingInfo) {
+		const queryParamForAvoidCache = `?t=${new Date().getTime()}`;
+		let recordingUrl = this.httpService.getRecordingMediaUrl(recording.recordingId);
+		recordingUrl += queryParamForAvoidCache;
+		this.actionService.openRecordingPlayerDialog(recordingUrl);
+	}
+
+	downloadRecording(recording: MeetRecordingInfo) {
+		const queryParamForAvoidCache = `?t=${new Date().getTime()}`;
+		let recordingUrl = this.httpService.getRecordingMediaUrl(recording.recordingId);
+		recordingUrl += queryParamForAvoidCache;
+
+		const link = document.createElement('a');
+		link.href = recordingUrl;
+		link.download = recording.filename || 'openvidu-recording.mp4';
+		link.dispatchEvent(
+			new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+				view: window
+			})
+		);
+
+		// For Firefox it is necessary to delay revoking the ObjectURL
+		setTimeout(() => link.remove(), 100);
+	}
+
+	async deleteRecording(recording: MeetRecordingInfo) {
 		try {
-			await this.httpService.deleteRecording(deleteRequest.recordingId!);
+			await this.httpService.deleteRecording(recording.recordingId!);
+			this.recordings = this.recordings.filter((r) => r.recordingId !== recording.recordingId);
 		} catch (error) {
 			console.log(error);
 		}
-
-		await this.loadRecordings();
 	}
 
 	private async loadRecordings() {
 		try {
-			const { recordings, continuationToken } = await this.httpService.getRecordings(this.continuationToken);
-			this.recordings = recordings;
-			this.continuationToken = continuationToken;
+			const response = await this.httpService.getRecordings(this.nextPageToken);
+			this.recordings.push(...response.recordings);
+			this.recordings = this.sortRecordingsByDate(this.recordings);
+			this.nextPageToken = response.pagination.nextPageToken;
+			this.moreRecordings = response.pagination.isTruncated;
 		} catch (error) {
 			console.log(error);
 		}
+	}
+
+	private sortRecordingsByDate(recordings: MeetRecordingInfo[]) {
+		return recordings.sort((a, b) => {
+			const dateA = new Date(a.startDate || -1);
+			const dateB = new Date(b.startDate || -1);
+			return dateA.getTime() - dateB.getTime();
+		});
+	}
+
+	private resetRecordings() {
+		this.recordings = [];
+		this.nextPageToken = undefined;
+		this.moreRecordings = false;
 	}
 }
