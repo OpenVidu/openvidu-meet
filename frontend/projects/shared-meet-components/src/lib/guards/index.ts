5ee89437b596a8dab01c5cc0e5833366daf6134d
juancarmore <juancar_more2@hotmail.com>
2025-05-31 00:13:50 +0200

    frontend: remove unused login and room creator components and associated guards

--- frontend/projects/shared-meet-components/src/lib/guards/index.ts
+++ frontend/projects/shared-meet-components/src/lib/guards/index.ts
@@ -3,7 +3,6 @@ export * from './auth.guard';
 export * from './extract-query-params.guard';
 export * from './moderator-secret.guard';
 export * from './participant-name.guard';
-export * from './room-creator.guard';
 export * from './run-serially.guard';
 export * from './validate-room-access.guard';
 export * from './validate-recording-access.guard';
