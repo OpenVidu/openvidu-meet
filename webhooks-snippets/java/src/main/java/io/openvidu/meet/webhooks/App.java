f8ba8bd0d11cceb71235aeaf49a52022b8c9ef3a
pabloFuente <pablofuenteperez@gmail.com>
2025-09-05 14:49:22 +0200

    Update Java webhook-snippet

--- webhooks-snippets/java/src/main/java/io/openvidu/meet/webhooks/App.java
+++ webhooks-snippets/java/src/main/java/io/openvidu/meet/webhooks/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MeetWebhooksApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MeetWebhooksApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
