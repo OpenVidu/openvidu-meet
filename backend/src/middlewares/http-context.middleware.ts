8f6af28bc224e702ad6e5670969649bad93fe56f
juancarmore <juancar_more2@hotmail.com>
2025-09-29 14:53:50 +0200

    backend: skip http context setting for LiveKit webhooks and remove clearing context

--- backend/src/middlewares/http-context.middleware.ts
+++ backend/src/middlewares/http-context.middleware.ts
@@ -2,14 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import { container } from '../config/dependency-injector.config.js';
 import { HttpContextService } from '../services/index.js';
 
-export const httpContextMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const httpContextMiddleware = (req: Request, _res: Response, next: NextFunction) => {
+	if (req.path === '/livekit/webhook') {
+		// Skip setting context for LiveKit webhooks
+		return next();
+	}
+
 	const httpContextService = container.get(HttpContextService);
 	httpContextService.setContext(req);
-
-	// Clear context after response is finished
-	res.on('finish', () => {
-		httpContextService.clearContext();
-	});
-
 	next();
 };
