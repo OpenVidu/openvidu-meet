0125ecf0e30b40f5467c5e2087109470bdf86a46
Carlos Santos <4a.santos@gmail.com>
2025-10-07 19:05:02 +0200

    feat: enhance development scripts with typings guard and improve watch behavior

--- scripts/dev/wait-for-typings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FLAG_PATH="./typings/dist/typings-ready.flag"
-
-# wait until the flag file exists
-while [ ! -f $FLAG_PATH ]; do sleep 0.1; done
-
-# then execute the real command passed as a parameter
-exec "$@"
