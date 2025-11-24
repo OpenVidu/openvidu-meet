c3ca84ad66ae755f907abe5be94c6290116fd0be
Carlos Santos <4a.santos@gmail.com>
2025-11-24 18:58:26 +0100

    frontend: remove unused meeting components plugins and action handler interfaces

--- meet-ce/frontend/projects/shared-meet-components/src/lib/customization/index.ts
+++ meet-ce/frontend/projects/shared-meet-components/src/lib/customization/index.ts
@@ -1,7 +1,1 @@
-/**
- * Index file for customization exports
- */
-export * from './components/meeting-components-plugins.token';
-export * from './handlers/meeting-action-handler';
-
 export * from './components/index';
