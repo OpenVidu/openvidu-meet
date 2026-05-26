1a805b83a87eae2c06cd5eff8193cd64660079f9
CSantosM <4a.santos@gmail.com>
2026-05-26 16:49:09 +0200

    frontend: add AppCeMeetingComponent and update exports in shared components

---
+++ meet-ce/frontend/projects/shared-meet-components/src/lib/domains/meeting/pages/app-ce-meeting/app-ce-meeting.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MeetingCustomLayoutComponent } from '../../customization/meeting-custom-layout/meeting-custom-layout.component';
+import { MeetingInvitePanelComponent } from '../../customization/meeting-invite-panel/meeting-invite-panel.component';
+import { MeetingParticipantItemComponent } from '../../customization/meeting-participant-item/meeting-participant-item.component';
+import { MeetingSettingsExtensionsComponent } from '../../customization/meeting-settings-extensions/meeting-settings-extensions.component';
+import { MeetingToolbarExtraButtonsComponent } from '../../customization/meeting-toolbar-extra-buttons/meeting-toolbar-extra-buttons.component';
+import { MeetingToolbarLeaveButtonComponent } from '../../customization/meeting-toolbar-leave-button/meeting-toolbar-leave-button.component';
+import { MeetingToolbarMoreOptionsMenuComponent } from '../../customization/meeting-toolbar-more-options-menu/meeting-toolbar-more-options-menu.component';
+import { MeetingComponent } from '../meeting/meeting.component';
+
+@Component({
+	selector: 'app-ce-ov-meeting',
+	imports: [
+		MeetingComponent,
+		MeetingToolbarLeaveButtonComponent,
+		MeetingToolbarExtraButtonsComponent,
+		MeetingInvitePanelComponent,
+		MeetingParticipantItemComponent,
+		MeetingCustomLayoutComponent,
+		MeetingToolbarMoreOptionsMenuComponent,
+		MeetingSettingsExtensionsComponent
+	],
+	template: `
+		<ov-meeting>
+			<ov-meeting-toolbar-leave-button />
+			<ov-meeting-toolbar-extra-buttons />
+			<ov-meeting-toolbar-more-options-menu />
+			<ov-meeting-invite-panel />
+			<ov-meeting-participant-item />
+			<ov-meeting-settings-extensions />
+			<ov-meeting-custom-layout />
+		</ov-meeting>
+	`,
+	styles: '',
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppCeMeetingComponent {}
