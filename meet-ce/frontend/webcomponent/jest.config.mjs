66bec30b2b2de9576211facbc8bc021465b09c59
CSantosM <4a.santos@gmail.com>
2026-06-18 16:45:47 +0200

    feat(tests): add Jest configuration and settings for webcomponent

---
+++ meet-ce/frontend/webcomponent/jest.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('jest').Config} */
+const config = {
+  displayName: 'webcomponent',
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  resolver: 'ts-jest-resolver',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['js', 'ts', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/tests/e2e/'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+  },
+  moduleNameMapper: {
+    '^@openvidu-meet/typings$': '<rootDir>/../../typings/src/index.ts',
+    '^@openvidu-meet/shared-components$': '<rootDir>/tests/__mocks__/shared-components.ts',
+    '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js'
+  }
+}
+
+export default config
